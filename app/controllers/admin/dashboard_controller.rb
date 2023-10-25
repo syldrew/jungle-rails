85692a8f2654e0bd77ba97dbc24a1147eeb446a4
syldrew <syl_peke14@hotmail.com>
2023-10-25 12:28:13 -0500

    fix bug

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
-  def show
-    @product_count = Product.count
-    @categories_count = Category.count
+    def show
+      @product_count = Product.count
+      @categories_count = Category.count
+    end
   end
-end
