b72e26b0d4bfaec5eb9e079cfc6af9798ee1f6e6
syldrew <syl_peke14@hotmail.com>
2023-10-26 13:24:16 -0500

    Refactoring , and implement visitors to register or log in with a user account

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# app/models/user.rb
+class User < ApplicationRecord
+    has_secure_password
+  
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
+  end
