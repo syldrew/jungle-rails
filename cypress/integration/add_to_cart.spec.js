09fc84e31ba9ed9a5f8e596193848b14213d6a05
syldrew <syl_peke14@hotmail.com>
2023-11-06 11:08:05 -0600

    Feature Spec: Add to Cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,20 @@
+const configYargs = require("webpack-cli/bin/config/config-yargs");
+
+describe('product_details.spec.js', () => {
+  it( 'should visit home', () => {
+    cy.visit('/')  
+}) 
+it("There is products on the page", () => {
+  cy.get(".products article").should("be.visible");
+});
+
+it("can visit one of the products", () => {
+  cy.get(".products article:first").click();
+});
+
+it("can add a product to cart", () => {
+  cy.get(".btn").click();
+});
+
+
+})
